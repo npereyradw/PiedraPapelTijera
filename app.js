5e406762bb799813dda3e201f82f544f43eafb1c
npereyradw <npereyradw@gmail.com>
2020-10-21 09:07:12 -0300

    Se finalizaron las funciones en Javascript

---
+++ app.js
@@ -0,0 +1,76 @@
+let userScore = 0;
+let computerScore= 0;
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
+const scoreBoard_section = document.querySelector(".score-board");
+const result_p = document.querySelector(".result > p");
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissor_div = document.getElementById("s");
+
+function getComputerChoice() {
+    const choices = ['r', 'p', 's'];
+    const randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber];
+}
+
+function convertToWord(letter) {
+    if(letter === "r") return "Piedra";
+    if(letter === "p") return "Papel";
+    return "Tijera";
+}
+
+function win(userChoice, computerChoice) {
+    userScore++;
+    userScore_span.innerHTML =userScore;
+    computerScore_span.innerHTML = computerScore;
+    result_p.innerHTML = `Elegiste ${convertToWord(userChoice)} y derrota a ${convertToWord(computerChoice)}(compu). ¡GANASTE!`;
+}
+
+function lose(userChoice, computerChoice) {
+    computerScore++;
+    userScore_span.innerHTML =userScore;
+    computerScore_span.innerHTML = computerScore;
+    result_p.innerHTML = `Elegiste ${convertToWord(userChoice)} y es débil contra ${convertToWord(computerChoice)}(compu). Perdiste...`;
+
+}
+
+function draw(userChoice, computerChoice) {
+        result_p.innerHTML = `Elegiste ${convertToWord(userChoice)} y empata con ${convertToWord(computerChoice)}(compu). Empate...`;
+
+}
+
+function game(userChoice){
+    const computerChoice = getComputerChoice();
+    switch (userChoice + computerChoice){
+        case "rs":
+        case "pr":
+        case "sp":
+            win(userChoice, computerChoice);
+            break;
+        case "rp":
+        case "ps":
+        case "sr":
+            lose(userChoice, computerChoice);
+            break;
+        case "rr":
+        case "pp":
+        case "ss":
+            draw(userChoice, computerChoice);
+    }
+}
+
+function main(){
+
+rock_div.addEventListener('click', function() {
+    game("r");
+})
+paper_div.addEventListener('click', function() {
+    game("p");
+})
+scissor_div.addEventListener('click', function() {
+    game("s");
+})
+}
+
+main();
